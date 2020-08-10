5af2ace5ee5094b5237fdbc45a8db6f3051e28cb
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-08-10 20:12:01 +0900

    add xamarin.forms project

--- xamarin.forms_project/xamarin.forms_project/MainPage.xaml.cs
+++ xamarin.forms_project/xamarin.forms_project/MainPage.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace xamarin.forms_project
@@ -13,6 +9,18 @@ namespace xamarin.forms_project
         public MainPage()
         {
             InitializeComponent();
+            for(int index = 1; index < 4; index++)
+            {
+                var image = new Image()
+                {
+                    Source = ImageSource.FromResource("xamarin.forms_project.images.image" + String.Format("{0:D3}", index) + ".jpg"),
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    WidthRequest = 200,
+                    HeightRequest = 100,
+                };
+                Layout.Children.Add(image);
+            }
         }
     }
 }
